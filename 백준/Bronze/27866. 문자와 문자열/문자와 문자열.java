ccc818ca2132c1443d16a875a4733c212816d8e9
y3ori <154998887+winy3ori@users.noreply.github.com>
2025-04-06 22:29:19 +0900

    [Bronze V] Title: 문자와 문자열, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -6,11 +6,7 @@ public class Main {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             String s = br.readLine();
             int i = Integer.parseInt(br.readLine());
-            char c = 0;
-            for (int j = 0; j < i; j++) {
-                c = s.charAt(j);
-            }
-            System.out.println(c);
+            System.out.println(s.charAt(i-1));
         }
     }
 }
