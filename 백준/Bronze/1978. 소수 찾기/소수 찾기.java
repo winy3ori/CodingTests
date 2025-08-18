9a2af0dcf0b9e4b0969b4557f4e932b1522e4ee2
y3ori <154998887+winy3ori@users.noreply.github.com>
2025-08-19 00:06:15 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            String[] nums = br.readLine().split(" ");
+
+            int count = 0;
+            for (String s : nums) {
+                int m = Integer.parseInt(s);
+                if (isPrime(m)) count++;
+            }
+
+            System.out.println(count);
+        }
+    }
+
+    private static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
